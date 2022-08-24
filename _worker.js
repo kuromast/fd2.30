43d2e924bef0adf9d30f3f51e22e7917ac5cc420
kuromast <105215044+kuromast@users.noreply.github.com>
2022-08-24 04:19:41 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
       const SingleDay = 'guaoo.herokuapp.com'
-      const DoubleDay = 'yahamas.herokuapp.com'
+      const DoubleDay = 'yamahas.herokuapp.com'
       let host = ''
       let nd = new Date();
       if (nd.getDate()%2) {
